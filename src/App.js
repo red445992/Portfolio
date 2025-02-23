062bf723754064f312060fc018cb7155cc44cb64
aditya <red445992@gmail.com>
2025-02-24 02:15:00 +0545

    navBar update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav.js";
 
+import About from "./about/About";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Nav />
-      </>
+      <Routes>
+        <Route path="/" element={About}></Route>
+      </Routes>
+    </Router>
   );
 };
 
