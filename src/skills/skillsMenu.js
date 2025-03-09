0ea73dd997a0133f566f58cce78095d45be11f68
aditya <red445992@gmail.com>
2025-03-09 13:02:56 +0545

    New Folder Added

--- src/skills/skillsMenu.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import "../styles/skillsMenu.css";
-export default class skillsMenu extends Component {
-  render() {
-    return (
-      <div>skillsMenu</div>
-    )
-  }
-}
