6740f56b6a7d86307c9c63ec5b70b99c3d2f5ae4
aditya <red445992@gmail.com>
2025-03-09 12:16:24 +0545

    About Section updated

---
+++ src/about/AboutSubheadingData.js
@@ -0,0 +1,50 @@
+const subheadingData = {
+    1: [
+        {
+            title: "About Me",
+            content: ( 
+                <>
+                    <p>
+                        Hi, I am Aditya.
+                    </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "Bachelor Degree",
+            content: (
+                <>
+                    <p>
+                        Computer Engineering
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "High School",
+            content: (
+                <>
+                    <p>
+                        Science Stream
+                    </p>
+                </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "Backend Developer",
+            content: (
+                <>
+                    <p>
+                        Experienced in Django.
+                    </p>
+                </>
+            ),
+        },
+    ],
+};
+
+export default subheadingData;
